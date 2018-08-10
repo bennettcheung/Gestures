02a59c9aa204f1867b6c6c788ef369311b7ecabe
Bennett <bennettcheung@yahoo.com>
2018-08-09 20:47:25 -0700

    Added SwipeGesture view controller

--- Gestures/SwipeViewController.m
+++ Gestures/SwipeViewController.m
@@ -10,6 +10,12 @@
 
 @interface SwipeViewController ()
 
+typedef NS_ENUM(NSInteger, OpenState) {OpenStateLeft, OpenStateNotOpen, OpenStateRight};
+
+@property (nonatomic, strong) UIView *whiteView;
+@property (nonatomic, strong) UIView *brownView;
+@property (nonatomic, assign) OpenState openstate;
+
 @end
 
 @implementation SwipeViewController
@@ -17,11 +23,96 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    CGFloat width = 600;
+    CGFloat height = 100;
+    CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width /2, CGRectGetMidY (self.view.bounds) - height / 2, width, height);
+    
+    self.brownView = [[UIView alloc] initWithFrame:frame];
+    self.brownView.backgroundColor = [UIColor brownColor];
+    self.brownView.clipsToBounds = YES;
+    [self.view addSubview:self.brownView];
+    
+    self.whiteView = [[UIView alloc] initWithFrame:frame];
+    self.whiteView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.whiteView];
+    
+    UISwipeGestureRecognizer *swipeRightRecognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeGuesture:)];
+    
+    UISwipeGestureRecognizer *swipeLeftRecognizer = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(swipeGuesture:)];
+    swipeLeftRecognizer.direction = UISwipeGestureRecognizerDirectionLeft;
+    
+    [self.whiteView addGestureRecognizer:swipeRightRecognizer];
+    [self.whiteView addGestureRecognizer:swipeLeftRecognizer];
+    
+    self.openstate = OpenStateNotOpen;
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)moveWhiteViewRIght {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.whiteView.frame = CGRectOffset(self.whiteView.frame, 200.0, 0.0);
+    }];
+}
+
+- (void)moveWhiteViewLeft {
+    [UIView animateWithDuration:0.5 animations:^{
+        self.whiteView.frame = CGRectOffset(self.whiteView.frame, -200.0, 0.0);
+    }];
+}
+
+-(void)swipeGuesture:(UISwipeGestureRecognizer *)sender{
+    
+    if (sender.state == UIGestureRecognizerStateEnded)
+    {
+        if (sender.direction == UISwipeGestureRecognizerDirectionLeft)
+        {
+
+            switch(self.openstate){
+                case OpenStateNotOpen:
+                    {
+                        self.openstate = OpenStateLeft;
+                        [self moveWhiteViewLeft];
+                    }
+                    break;
+                case OpenStateRight:
+                    {
+                    self.openstate = OpenStateNotOpen;
+                        [self moveWhiteViewLeft];
+                    }
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        else if  (sender.direction == UISwipeGestureRecognizerDirectionRight)
+        {
+            switch (self.openstate) {
+                case OpenStateNotOpen:
+                    {
+                        self.openstate = OpenStateRight;
+                        [self moveWhiteViewRIght];
+                    }
+                    break;
+                case OpenStateLeft:
+                    {
+                    self.openstate = OpenStateNotOpen;
+                    [self moveWhiteViewRIght];
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
 }
 
 /*
